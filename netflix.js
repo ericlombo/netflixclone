0dd5d4e87cab0998deee0b0da9feb32ba9c607ed
ericlombo <ericlombo1999@gmail.com>
2023-05-30 12:48:31 +0300

    modification

--- netflix.js
+++ netflix.js
@@ -1,4 +1,6 @@
 const parentlist = document.querySelector('.parentlist')
+const section = document.querySelector('.container')
+
 parentlist.addEventListener('click', function(e){
     if (e.target.tagName=== 'LI'){
         const inside=e.target.querySelector('.inside')
@@ -6,3 +8,26 @@ parentlist.addEventListener('click', function(e){
     }
 
 })
+
+async function fetchData() {
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await response.json()
+    console.log(data)
+    data.results.forEach(function (character) {
+        const container = document.createElement('div')
+        container.classList.add('char-card')
+        container.innerHTML = `
+            <div class="cardimg"><img src="${character.image}"/></div>
+            <h2>${character.name}</h2>
+            <h3>${character.gender}</h3>
+            ${character.origin.name}
+            ${character.location.name}
+            ${character.status}
+            ${character.species}
+        `
+
+        section.appendChild(container)
+    })
+}
+
+fetchData()
